db445fe24eec77dfb48469babab4c3150cbeae36
yejianbin233 <yejianbin233@163.com>
2022-09-25 18:47:06 +0800

    first commit

---
+++ Source/AI_Project/AI_Project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AI_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AI_Project, "AI_Project" );
---
+++ Source/AI_Project/AI_ProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AI_ProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AI_PROJECT_API AAI_ProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
