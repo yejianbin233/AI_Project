c43381ca82634c51bb2c3c9e61c1bff191de2718
yejianbin233 <yejianbin233@163.com>
2023-05-14 09:19:11 +0800

    本地文件提交

---
+++ Source/AI_Project/Camera/RST_PlayerCameraManager.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/PlayerCameraManager.h"
+#include "RST_PlayerCameraManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AI_PROJECT_API ARST_PlayerCameraManager : public APlayerCameraManager
+{
+	GENERATED_BODY()
+};
---
+++ Source/AI_Project/PlayerController/RST_PlayerController.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "RST_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AI_PROJECT_API ARST_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+
+public:
+
+	
+};
